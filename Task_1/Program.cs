3545a971957136383631c4e1956df1d069240e65
Diana Giniyatullina <disshh09022004@gmail.com>
2024-01-25 22:03:05 +0300

    add some variables

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("heelloo he");
+﻿//Console.WriteLine("heelloo he");
+
+int cifra = 100;
+Console.WriteLine(cifra);
+
+double f = 5.12;
+char c = '?';
+bool b = false;
+Console.WriteLine(f);
+Console.WriteLine(c);
+Console.WriteLine(b);
