09086325d14ab26f56d6b205cc4ef358a0db6189
Diana Giniyatullina <disshh09022004@gmail.com>
2024-01-26 00:06:46 +0300

    make task4 in less1

---
+++ Task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 4;
+int b = 9;
+int c = 45;
+int d = 78;
+int i = 1;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (i > max)
+{
+    max = i;
+}
+Console.WriteLine(max);
