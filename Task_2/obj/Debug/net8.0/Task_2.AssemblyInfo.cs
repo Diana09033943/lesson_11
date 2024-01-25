589a8a48121042cf0490181e12d44d2895ef170c
Diana Giniyatullina <disshh09022004@gmail.com>
2024-01-25 23:28:58 +0300

    make some informations in task2:lesson1

--- Task_2/obj/Debug/net8.0/Task_2.AssemblyInfo.cs
+++ Task_2/obj/Debug/net8.0/Task_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3545a971957136383631c4e1956df1d069240e65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86a7146340439788b1793f56fea2a20b9356d2e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
